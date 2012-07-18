7f8d03568dbe059cecb081f2e750562a361997ea
Ryan Koven <celeritas@ucla.edu>
2012-07-17 23:11:13 -0700

    Added functions

--- src/div_and_mult.h
+++ src/div_and_mult.h
@@ -1,5 +1,5 @@
 /*
- Ryan Koven 7/1/2012
+ Ryan Koven 2012
  div_and_mult.h: c++ header file that defines several functions that implement several new and old algorithms for multiplying and dividing integers. 
  References: Dasgupta, Papadimitriou, Vazirani: Algorithms. 2008.
 */
@@ -10,10 +10,117 @@
 #define DIV_AND_MULT_H_
 
 
-#include<iostream> // Will be necessary later
+#include<iostream> 
 #include<cmath>
 
+namespace arithmetic{
 
+
+/* division: Implements an interesting (but not entirely fast-- O(n^2), n the number of bits in the representation of the integers) 
+   method to divide x by y-- x, y nonnegative, y > 0. Returns quotient and remainder in an array with quotient in the first position 
+   in the array, remainder in second. */
+int* divide(int x, int y){
+
+	assert(y > 0);
+
+	int q_r[2];
+
+	q_r[0] = 0;
+	q_r[1] = 0;
+
+	if (x > 0){
+		q_r[0] = 2*divide(x/2, y)[0];
+		q_r[1] = 2*divide(x/2, y)[1];
+
+		if (x%2 != 0)
+			q_r[1] += 1;
+		
+		if (q_r[1] >= y)
+			q_r[1] -= y , q_r[0] += 1;
+	}
+	 
+	return q_r;
+}
+
+/* multiply: An interesting method to multiply integers. Again, not entirely fast. */
+int multiply(int x , int y){
+
+
+	int product = 0;
+
+	if (x >= 0 && y > 0){
+
+		int z = multiply(x, y/2);
+
+		if (y%2 == 0)
+			product = 2*z;
+		else
+			product = x + 2*z;
+	}
+
+	return product;
+
+}
+
+/* modexp: Modular exponentiation-- returns x^y mod N for integers x, y, N. Returns 0 if any of x, y, N are negative. */
+int modexp(int x, int y, int N){
+
+	assert(N > 0);
+
+	int exp = 0;
+
+	if (x >= 0 && y >= 0 && N > 1){
+
+		if (y == 0)
+			exp = 1;
+		else{
+			int z = modexp(x, y/2, N);
+			if (y%2 == 0)
+				exp = z*z % N;
+			else
+				exp = x*z*z % N; 
+	}
+
+	}
+	return exp;  
+
+}
+
+/* Implementation of Euclid's >2000 year old algorithm for finding the greatest common divisor of two integers a, b. 
+   Returns gcd(a, b) if a>=b>=0, 0 otherwise. */
+ int euclid_gcd(int a, int b){
+
+	 int gcd = 0;
+
+	 if (a >= b && b >= 0){
+		 if (b == 0)
+			 gcd = a;
+		 else
+			 gcd = euclid_gcd(b, a%b);
+	 }
+
+	 return gcd;
+ }
+
+ /* extended_euclid: Returns integers x, y, d such that d = gcd(a, b) (a >=b >= 0) and d = ax + by in an array. 
+    The algorithm is an extension of Euclid's algorithm from euclid_gcd, made possible by the fact that gcd(a, b)
+	can always be expressed as ax + by for some integers x, y. */
+ int* extended_euclid_gcd(int a, int b){
+
+	 int x_y_d[3];
+
+	 for (int i = 0; i < 3; i++)
+		 x_y_d[i] = 0;
+
+	 if (b == 0)
+		 x_y_d[0] = 1, x_y_d[1] = 0, x_y_d[2] = a;
+	 else{
+		x_y_d[0] = extended_euclid_gcd(b, a%b)[1]; 
+		x_y_d[1] = extended_euclid_gcd(b, a%b)[0] - (a/b)*extended_euclid_gcd(b, a%b)[1];
+		x_y_d[2] = extended_euclid_gcd(b, a%b)[2];
+	 }
+	 return x_y_d;
+ }
 
 //Calculate the product a x b of two n digit numbers a, b recursively 
 long long Recursive_Mult(int x, int y){
@@ -84,5 +191,6 @@ long long Karatsuba_Mult(int x, int y){
 	return product;
 }
 
+}
 
 #endif
